e54a057bfc31ceb141f32dc2d0a3a5843ffa4897
hanyun <1355081829@qq.com>
2020-11-06 11:08:57 +0800

    增加公共头部

--- src/components/AdminLayout.tsx
+++ src/components/AdminLayout.tsx
@@ -1,5 +1,5 @@
 import React, {Component, ReactNode} from 'react'
-import {Layout, Spin} from 'antd';
+import {Breadcrumb, Layout, Spin} from 'antd';
 import {
     MenuUnfoldOutlined,
     MenuFoldOutlined,
@@ -8,8 +8,9 @@ import LeftBar from './LeftBar';
 import {withRouter, matchPath, RouteComponentProps} from 'react-router-dom'
 import {connect} from 'react-redux'
 import {IRoute, PermissionState} from '../store/states/PermissionState'
+import TopHeader from "./TopHeader";
 
-const {Header, Sider, Content} = Layout;
+const {Sider, Content} = Layout;
 
 interface IAdminLayoutState {
     collapsed: boolean
@@ -68,27 +69,43 @@ class AdminLayout extends Component<IAdminLayoutProps, IAdminLayoutState> {
         return (
             <>
                 <Layout>
-                    <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
-                        <LeftBar permissionList={this.props.permissionList}/>
-                    </Sider>
-                    <Layout className="site-layout">
-                        <Header className="site-layout-background" style={{padding: 0}}>
-                            {React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {
-                                className: 'trigger',
-                                onClick: this.toggle,
-                            })}
-                        </Header>
-                        <Content
-                            className="site-layout-background"
-                            style={{
-                                margin: '24px 16px',
-                                padding: 24,
-                                minHeight: 280,
-                            }}
-                        >
-                            {this.props.children}
-                        </Content>
+                    <TopHeader/>
+                    <Layout>
+
+                        <Sider trigger={null} collapsible collapsed={this.state.collapsed}>
+                            <LeftBar permissionList={this.props.permissionList}/>
+                        </Sider>
+                        <Layout className="site-layout">
+                            {/*{React.createElement(this.state.collapsed ? MenuUnfoldOutlined : MenuFoldOutlined, {*/}
+                            {/*    className: 'trigger',*/}
+                            {/*    onClick: this.toggle,*/}
+                            {/*})}*/}
+                            <span className={'trigger'} onClick={this.toggle} style={{marginLeft:'8px',float:"left",height:'100%'}}>
+                                {
+                                    this.state.collapsed?
+                                        <MenuUnfoldOutlined/>
+                                        :
+                                        <MenuFoldOutlined/>
+                                }
+                            </span>
+                            <Breadcrumb style={{ margin: '8px 16px' }}>
+                                <Breadcrumb.Item>Home</Breadcrumb.Item>
+                                <Breadcrumb.Item>List</Breadcrumb.Item>
+                                <Breadcrumb.Item>App</Breadcrumb.Item>
+                            </Breadcrumb>
+                            <Content
+                                className="site-layout-background"
+                                style={{
+                                    margin: '24px 16px',
+                                    padding: 24,
+                                    minHeight: 280,
+                                }}
+                            >
+                                {this.props.children}
+                            </Content>
+                        </Layout>
                     </Layout>
+
                 </Layout>
             </>
         )
---
+++ src/components/TopHeader.tsx
@@ -0,0 +1,31 @@
+import React, {Component} from "react";
+import {Dropdown, Layout, Menu} from "antd";
+const {Header} = Layout;
+
+const menu = (
+    <Menu>
+        <Menu.Item key="1">
+            1st menu item
+        </Menu.Item>
+    </Menu>
+)
+class TopHeader extends Component<any, any> {
+    render() {
+        return (
+            <>
+                <Header className="header">
+                    <div className="logo" />
+                    <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
+                        <Menu.Item key="1">nav 1</Menu.Item>
+                        <Menu.Item key="2">nav 2</Menu.Item>
+                        <Menu.Item key="3">nav 3</Menu.Item>
+                    </Menu>
+                    {/*<Dropdown.Button overlay={menu} placement="bottomCenter">*/}
+                    {/*    Dropdown*/}
+                    {/*</Dropdown.Button>*/}
+                </Header>
+            </>
+        );
+    }
+}
+export default TopHeader
