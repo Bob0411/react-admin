5c5dabecbd0e85bee0cc1789e2610481d726d92c
hanyun <1355081829@qq.com>
2020-11-23 20:58:33 +0800

    选项管理

--- src/pages/Catalog/Option/OptionDetail.tsx
+++ src/pages/Catalog/Option/OptionDetail.tsx
@@ -2,7 +2,8 @@ import React, {Component} from "react";
 import {withRouter} from "react-router-dom";
 import {RouteComponentProps} from "react-router";
 import {getOptionDetail, getOptionList} from "../../../api/option";
-import {Form, Input, Select} from "antd";
+import {Button, Form, Input, Select, Space} from "antd";
+import {MinusCircleOutlined, PlusOutlined} from '@ant-design/icons';
 
 interface IProps extends RouteComponentProps {
     optionId?: number
@@ -56,6 +57,9 @@ class OptionDetail extends Component<IProps, IState> {
             })
         })
     }
+    getFormValue = (value: any) => {
+        console.log(value)
+    }
 
     render() {
         return (
@@ -65,8 +69,9 @@ class OptionDetail extends Component<IProps, IState> {
                         <Form
                             initialValues={{
                                 name: this.state.option.name,
-                                description: this.state.option.description
+                                description: this.state.option.description,
                             }}
+                            onFinish={this.getFormValue}
                         >
                             <Form.Item
                                 name='name'
@@ -88,14 +93,55 @@ class OptionDetail extends Component<IProps, IState> {
                                     {
                                         this.state.optionList.map((option: IOption) => {
                                             return (
-                                                <Select.Option value={option.id}>{option.name}</Select.Option>
+                                                <Select.Option
+                                                    value={option.id}
+                                                    key={option.id}
+                                                >
+                                                    {option.name}
+                                                </Select.Option>
                                             )
                                         })
                                     }
                                 </Select>
                             </Form.Item>
-
-
+                            <Form.List name="optionList">
+                                {(fields, {add, remove}) => (
+                                    <>
+                                        {fields.map(field => (
+                                            <Space key={field.key} style={{display: 'flex', marginBottom: 8}}
+                                                   align="baseline">
+                                                <Form.Item
+                                                    {...field}
+                                                    label='选项名称'
+                                                    name={[field.name, 'optionName']}
+                                                    fieldKey={[field.fieldKey, 'first']}
+                                                    rules={[{required: true, message: '选项名称'}]}
+                                                >
+                                                    <Input placeholder="选项名称" allowClear/>
+                                                </Form.Item>
+                                                <Form.Item
+                                                    {...field}
+                                                    label='选项值'
+                                                    name={[field.name, 'optionValue']}
+                                                    fieldKey={[field.fieldKey, 'last']}
+                                                    rules={[{required: true, message: '选项值'}]}
+                                                >
+                                                    <Input placeholder="选项值" allowClear/>
+                                                </Form.Item>
+                                                <MinusCircleOutlined onClick={() => remove(field.name)}/>
+                                            </Space>
+                                        ))}
+                                        <Form.Item>
+                                            <Button type="dashed" onClick={() => add()} icon={<PlusOutlined/>}>
+                                                添加选项
+                                            </Button>
+                                        </Form.Item>
+                                    </>
+                                )}
+                            </Form.List>
+                            <Form.Item>
+                                <Button type='primary' htmlType='submit'>确定</Button>
+                            </Form.Item>
                         </Form>
                         :
                         null
